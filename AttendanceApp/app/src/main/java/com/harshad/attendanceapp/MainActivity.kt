29a5f55725c4e112a8c36e377b6dfc6c7682f8b7
SpartanHarshad <harshadpawar8555@gmail.com>
2023-04-08 18:47:34 +0530

    project dependecy setup

---
+++ AttendanceApp/app/src/main/java/com/harshad/attendanceapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.harshad.attendanceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.harshad.attendanceapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
